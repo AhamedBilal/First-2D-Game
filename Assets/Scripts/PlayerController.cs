15a62968ea91e23081121e287683d6dc8f4d95f3
Ahamed Bilal <bilalazmy0@gmail.com>
2019-09-17 06:41:22 +0530

    1.0.0

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private float movement = 0f;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (movement < 0f)
+        {
+            rigidbody.velocity = new Vector2(movement*speed, rigidbody.velocity.y);
+        }
+        if (movement > 0f)
+        {
+            rigidbody.velocity = new Vector2(movement * speed, rigidbody.velocity.y);
+        }
+    }
+}
